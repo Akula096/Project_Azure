d4a9db740adbfe92f4d0f0872906ca3ea5656833
Akula Sathish <Akula.Sathish+CPSI@trubridge.com>
2026-06-05 07:19:46 -0400

    updated the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group
+  location = var.location
+}
 
+resource "azurerm_virtual_network" "virtual_network" {
+   name = var.virtual_network
+   address_space = var.range
+   location = var.location
+   resource_group_name = var.resource_group  
+}
+
+
+resource "azurerm_subnet" "subnet" {
+    name = var.subnet
+    resource_group_name = var.resource_group
+    virtual_network_name = var.virtual_network
+    address_prefixes = var.address_prefixes
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,28 @@
+variable "resource_group" {
+    type = string
+    default = "Devops_RG"
+}
 
+variable "location" {
+   type = string
+   default = "East US"  
+}
+
+variable "virtual_network" {
+    type = string
+    default = "VNET-1"
+}
+
+variable "range" {
+    type = list(string)
+    default = ["10.208.0.0/16"]
+}
+
+variable "subnet" {
+   type = string
+   default = "SUBNET-1"  
+}
+variable "address_prefixes" {
+   type = list(string)
+   default = ["10.208.0.0/24" ]
+}
