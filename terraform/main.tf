5b43e28ce76ef58489898712d5d76d7ca8470cf0
Akula Sathish <Akula.Sathish+CPSI@trubridge.com>
2026-06-10 08:25:40 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,13 @@ resource "azurerm_virtual_network" "virtual_network" {
    name = var.virtual_network
    address_space = var.range
    location = var.location
-   resource_group_name = var.resource_group  
+   resource_group_name = azurerm_resource_group.rg.name
 }
 
 
 resource "azurerm_subnet" "subnet" {
     name = var.subnet
-    resource_group_name = var.resource_group
+    resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = var.virtual_network
     address_prefixes = var.address_prefixes
 }
